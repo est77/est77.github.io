7711dbeef3cc9da83b38b0a297b7313be55009f9
est77 <ramenhdr@gmail.com>
2015-05-05 00:38:01 +0200

    Initial content upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+function createHeader()
+{
+    document.write( '<div class="small-12 medium-12 large-12 small-centered columns">');
+    document.write( '    <header>');
+    document.write( '        <h1><a href="index.html">Esteban Tovagliari</a></h1>');
+    document.write( '        <h2><a href="index.html">VFX Software Development</a></h2>');
+    document.write( '    </header>');
+    document.write( '</div>');
+}
+
+function createFooter()
+{
+    document.write( '<div class="row">');
+    document.write( '    <div class="small-12 medium-12 large-12 columns">');
+    document.write( '        <footer>');
+    document.write( '            <ul>');
+    document.write( '                <li>&copy; 2015 <a href="index.html">Esteban Tovagliari</a>. All rights reserved.</li>');
+    document.write( '                <li><a href="mailto:est.tovagliari@gmail.com"><i class="fa fa-envelope-o"></i> est.tovagliari@gmail.com</a></li>');
+    document.write( '                <!-- *****************************************************************************************');
+    document.write( '                The "Powered by Type & Grids" link in the footer is required in the Free version');
+    document.write( '                typeandgrids.com/pirates -->');
+    document.write( '                <li>Powered by <a href="http://www.typeandgrids.com" target="_blank">Type &amp; Grids</a></li>');
+    document.write( '                <!--  ************************************************************************************ -->');
+    document.write( '            </ul>');
+    document.write( '            <div class="social-icons">');
+    document.write( '                <a href="https://github.com/est77" target="_blank"><i class="fa fa-github-square fa-2x"></i></a>');
+    document.write( '                <a href="https://twitter.com/EstTovagliari" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a>');
+    document.write( '            </div>');
+    document.write( '        </footer>');
+    document.write( '    </div>');
+    document.write( '</div>');
+}
